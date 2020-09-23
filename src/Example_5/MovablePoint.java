991527d110c012ebf1c06bce72a399e9481974b5
Appjey <Loikoanton@yandex.ru>
2020-09-23 19:53:16 +0300

    :D

--- src/Example_5/MovablePoint.java
+++ src/Example_5/MovablePoint.java
@@ -4,9 +4,4 @@ public class MovablePoint extends MovableRectangle implements Movable {
     public MovablePoint(int x1, int y1, int x2, int y2) {
         super(x1, y1, x2, y2);
     }
-
-    @Override
-    public void moveUP() {
-
-    }
 }
